c953e2936cb80b1d1473e47dfe06579cf64f8be4
Gursewak Braitch <g.braitch@hotmail.com>
2020-07-16 17:45:04 -0700

    second commit -_-

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.get('/info', (req,res) => {
   res.send(`<p>Phonebook has info for ${phonebook.length} people</p>` + new Date())
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
